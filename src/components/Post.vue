fb1ec5b5cd9535c0dbd73f272c4ba75200859314
aspynlyn <lyn.aspyn@gmail>
2025-07-02 21:24:16 +0900

    [docs]:보충수업

---
+++ src/components/Post.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const props = defineProps({
+  item: Object,
+});
+
+const json = JSON.stringify(props.item);
+
+const moveToDetail = () => {
+  router.push({
+    path: '/detail',
+    state: {
+      data: json,
+    },
+  });
+};
+// 라우팅 처리 /detail로 이동
+// props.item을 JSON 문자열로 변경하여 state.data에 대입
+</script>
+
+<template>
+  <div class="card mb-3 pointer" @click="moveToDetail">
+    <div class="card-body">
+      <h3 class="card-title">{{ props.item.title }}</h3>
+      <div class="card-subtitle text-muted">{{ props.item.body }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Detail.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Post from '@/components/Post.vue';
+import { reactive, onMounted } from 'vue';
+
+const state = reactive({
+  post: {
+    id: 0,
+    title: '',
+    body: '',
+    userId: '',
+  },
+});
+
+onMounted(() => {
+  const passJson = history.state.data;
+  if (passJson) {
+    state.post = JSON.parse(passJson);
+  }
+});
+</script>
+
+<template>
+  <h1>Detail.vue</h1>
+  <post :item="state.post"></post>
+</template>
+
+<style scoped></style>
